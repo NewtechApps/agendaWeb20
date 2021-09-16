2dbc6026556bef161f9376b26065a12732ba8dc1
Deivys Joenck <34344926+deivysjoenck@users.noreply.github.com>
2021-09-16 20:16:57 -0300

    Login de Usuários
    - Inclusão de regra para não permitir login para usuários inativos.

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -62,61 +62,12 @@ class LoginController extends Controller
         ]);
     }
 
-/*
-    protected function login(Request $request)
-    {
 
-        $this->validateLogin($request);
-
-        // If the class is using the ThrottlesLogins trait, we can automatically throttle
-        // the login attempts for this application. We'll key this by the username and
-        // the IP address of the client making these requests into this application.
-        if (method_exists($this, 'hasTooManyLoginAttempts') &&
-            $this->hasTooManyLoginAttempts($request)) {
-            $this->fireLockoutEvent($request);
-
-            return $this->sendLockoutResponse($request);
-        }
-
-
-        $usuario = DB::table('usuario')->where('login', '=', $request->login)->first();
-        if(!$usuario){
-
-            if ($this->attemptLogin($request)) {
-                return $this->sendLoginResponse($request);
-            }
-
-            // If the login attempt was unsuccessful we will increment the number of attempts
-            // to login and redirect the user back to the login form. Of course, when this
-            // user surpasses their maximum number of attempts they will get locked out.
-            $this->incrementLoginAttempts($request);
-            return $this->sendFailedLoginResponse($request);
     
-        } else {
-
-            if($usuario->reset_password){
-                if ($this->attemptLogin($request)) {
-                    return $this->sendLoginResponse($request);
-                } 
-
-                // If the login attempt was unsuccessful we will increment the number of attempts
-                // to login and redirect the user back to the login form. Of course, when this
-                // user surpasses their maximum number of attempts they will get locked out.
-                $this->incrementLoginAttempts($request);
-                return $this->sendFailedLoginResponse($request);
-
-            } else {
-                return view('auth.passwords.email')->with('usuario', $usuario);
-            }
-        }
-    }
-*/
-    public function logout(Request $request) 
+    protected function credentials(Request $request)
     {
-        Auth::logout();
-        return redirect('/');
+        return array( 'login' => $request->login, 'password' => $request->password, 'status' => '0' );
     }
-
 
     protected function authenticated(Request $request, $user)
     {
